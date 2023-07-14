9fd2bd3f0f6883c5d59ea34fe794fbb71287c1ab
premSuperdev <108412360+premSuperdev@users.noreply.github.com>
2023-07-14 09:38:39 +0700

    update

--- api/index.js
+++ api/index.js
@@ -5,7 +5,36 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.get('/2', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/3', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/4', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/5', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/6', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/7', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/8', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/9', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/10', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/11', (req, res) => {
+  res.send('Hello World!')
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
